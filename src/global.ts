9d1d87dc3b7c3b1369c76662a7c5d0f8f1632414
Paras Shah <parasshah195@gmail.com>
2025-02-12 02:11:55 +0530

    Add gsap library and scrolltrigger plugin globally

--- src/global.ts
+++ src/global.ts
@@ -1,5 +1,12 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import { animatedDetailsAccordions } from '$components/accordions';
 import { dialogInit } from '$components/dialog';
+
+window.gsap = gsap;
+window.ScrollTrigger = ScrollTrigger;
+window.gsap.registerPlugin(ScrollTrigger);
 
 window.Webflow = window.Webflow || {};
 window.Webflow?.push(() => {
