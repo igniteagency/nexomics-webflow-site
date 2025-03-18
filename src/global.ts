996b670ffb454e5cf20f02b20ccc866c991fd6db
Paras Shah <parasshah195@gmail.com>
2025-03-19 02:34:18 +0530

    Fix fade animation not rendering on DOM content height change
    caused due to CMS Filter usage

--- src/global.ts
+++ src/global.ts
@@ -18,6 +18,20 @@ window.Webflow?.push(() => {
   fadeUp();
 });
 
+window.fsAttributes = window.fsAttributes || [];
+window.fsAttributes.push([
+  'cmsfilter',
+  (filterInstances) => {
+    const [filterInstance] = filterInstances;
+    filterInstance.listInstance.on('renderitems', (renderedItems) => {
+      // refresh scrolltrigger after cmsfilter items are rendered
+      setTimeout(() => {
+        window.ScrollTrigger.refresh();
+      }, 100);
+    });
+  },
+]);
+
 const CURRENT_YEAR = document.getElementById('current-year');
 
 if (CURRENT_YEAR) {
